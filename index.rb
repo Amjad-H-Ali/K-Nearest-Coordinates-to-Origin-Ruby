fa1c384d31d88503ae9c6493af33c5f54edd6afe
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-02-07 15:37:18 -0600

    Defined function that sorts array into max heap

--- index.rb
+++ index.rb
@@ -1,2 +1,38 @@
 p "K Nearest Point(s) to Origin!"
 
+# Function that will sort an array into a Max-Heap
+=begin
+	Params:
+		@arr -> array that will be sorted
+		@indx -> index of current parent element in array
+		@size -> size of array
+=end
+def max_heap arr, indx, size
+
+	# Assume largest int is the parent node.
+	# Store its index
+	largest = indx
+
+	# Initialize left and right children indexes
+	left = (indx * 2) + 1
+
+	right = left + 1
+
+	# Reinitialize largest index if left is greater
+	if left < size and  arr[left] > arr[largest]
+		largest = left
+
+	# Reinitialize largest index if right is greater 
+	if right < size and arr[right] > arr[largest]
+		largest = right
+
+	# Swap largest with parent node in array if parent is not the greatest
+	# Repeat for children node using reccursion
+	if largest != indx
+		swap(arr, largest, indx)
+
+		max_heap(arr, largest, size)
+				
+
+
+
