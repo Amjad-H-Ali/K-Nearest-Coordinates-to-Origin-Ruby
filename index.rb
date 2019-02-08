3bc130520fe3d8e51e7f551c468b30e0e545ed9e
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-02-08 00:36:12 -0600

    Fixed a couple of syntax errors and function returns k nearest coordinates to origin

--- index.rb
+++ index.rb
@@ -22,12 +22,14 @@ def k_nearest_coor coor, k
 	# Compare each distance with the root in k portion of array
 	# If less than, swap. Reheapify k portion of array
 	for indx in k...coor.length
-		if coor[indx][0]**2 + coor[indx][1]**2 > coor[0][0] + coor[0][1]
+		if (coor[indx][0]**2 + coor[indx][1]**2) < (coor[0][0]**2 + coor[0][1]**2)
 
 			swap(coor, 0, indx)
 			max_heap(coor, 0, k)
 		end
-	end		
+	end	
+	# Return k portion of array
+	coor[0...k]	
 end
 
 
@@ -54,10 +56,12 @@ def max_heap arr, indx, size
 	# Reinitialize largest index if left coordinate is further away from the origin
 	if left < size and  (arr[left][0]**2 + arr[left][1]**2) > (arr[largest][0]**2 + arr[largest][1]**2)
 		largest = left
+	end	
 
 	# Reinitialize largest index if right coordinate is further away from origin
-	if right < size and (arr[right][0]**2 + arr[right][1]**2) > arr[largest][0]**2 + arr[largest][1]**2)
+	if right < size and (arr[right][0]**2 + arr[right][1]**2) > (arr[largest][0]**2 + arr[largest][1]**2)
 		largest = right
+	end	
 
 	# Swap largest with parent node in array if parent is not the greatest
 	# Repeat for children node using reccursion
@@ -65,8 +69,19 @@ def max_heap arr, indx, size
 		swap(arr, largest, indx)
 
 		max_heap(arr, largest, size)
+	end	
 end
 
 
+# Tuple representing points on a Cartesian coordinate plane 
+coordinates = [[-2, -4], [0, -2], [-1, 0], [3, -5], [-2, -3], [3, 2], [11, 2], [8, 0], [4, 5], [15, 2], [9, -9], [0,0]]
 
+coordinates2 = [[8,9], [0, 0], [4, -4], [1, 1], [3, 9], [1, -1]]
+								
+								
+							  # **OUTPUT DOES NOT NEED TO BE IN ORDER**
+									
+p k_nearest_coor(coordinates, 5)  # >> [(0, 0), (-1, 0), (0, -2), (-2, -3), (3, 2)]
+
+p k_nearest_coor(coordinates2, 3) # >> [(0,0), (1, 1), (1, -1)]
 
